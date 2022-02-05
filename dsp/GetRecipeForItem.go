a1eaba629049e6ca77c84624bd0a1d66f1ffecb8
Alex Nou <alexnou@gmail.com>
2022-02-05 01:31:59 -0700

    Handle NaN

--- dsp/GetRecipeForItem.go
+++ dsp/GetRecipeForItem.go
@@ -1,6 +1,9 @@
 package dsp
 
-import "addi/models"
+import (
+	"addi/models"
+	"math"
+)
 
 func GetRecipeForItem(itemName string, craftingSpeed float64, parentItemName string) []*models.Recipe {
 	recipes := []*models.Recipe{}
@@ -12,6 +15,9 @@ func GetRecipeForItem(itemName string, craftingSpeed float64, parentItemName str
 
 		for _, material := range item.Materials {
 			consumedMats[material.Name] = material.Count * numberOfFacilitiesNeeded / item.Time
+			if math.IsNaN(consumedMats[material.Name]) {
+				consumedMats[material.Name] = 0
+			}
 		}
 
 		recipe := &models.Recipe{
@@ -27,6 +33,9 @@ func GetRecipeForItem(itemName string, craftingSpeed float64, parentItemName str
 
 		for _, material := range item.Materials {
 			targetCraftingSpeed := numberOfFacilitiesNeeded * material.Count / item.Time
+			if math.IsNaN(targetCraftingSpeed) {
+				targetCraftingSpeed = 0
+			}
 			materialRecipe := GetRecipeForItem(material.Name, targetCraftingSpeed, item.Name)
 			recipes = append(recipes, materialRecipe...)
 		}
