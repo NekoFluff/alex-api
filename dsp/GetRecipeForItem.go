742e0460d344169ac3300a79be815c88fe4936a6
CrazyFluff <alexnou@gmail.com>
2022-02-11 19:20:55 -0700

    Null fixes

--- dsp/GetRecipeForItem.go
+++ dsp/GetRecipeForItem.go
@@ -11,12 +11,17 @@ func GetRecipeForItem(itemName string, craftingSpeed float64, parentItemName str
 
 	if ok {
 		consumedMats := make(map[string]float64)
-		numberOfFacilitiesNeeded := *item.Time * craftingSpeed / *item.Produce
+		numberOfFacilitiesNeeded := 0.0
+		if item.Produce != nil && item.Time != nil {
+			numberOfFacilitiesNeeded = *item.Time * craftingSpeed / *item.Produce
+		}
 
 		for _, material := range item.Materials {
-			consumedMats[*material.Name] = *material.Count * numberOfFacilitiesNeeded / *item.Time
-			if math.IsNaN(consumedMats[*material.Name]) {
-				consumedMats[*material.Name] = 0
+			if material.Name != nil && item.Time != nil {
+				consumedMats[*material.Name] = *material.Count * numberOfFacilitiesNeeded / *item.Time
+				if math.IsNaN(consumedMats[*material.Name]) {
+					consumedMats[*material.Name] = 0
+				}
 			}
 		}
 
@@ -32,12 +37,17 @@ func GetRecipeForItem(itemName string, craftingSpeed float64, parentItemName str
 		recipes = append(recipes, recipe)
 
 		for _, material := range item.Materials {
-			targetCraftingSpeed := numberOfFacilitiesNeeded * *material.Count / *item.Time
+			targetCraftingSpeed := 0.0
+			if material.Count != nil && item.Time != nil {
+				targetCraftingSpeed = numberOfFacilitiesNeeded * *material.Count / *item.Time
+			}
 			if math.IsNaN(targetCraftingSpeed) {
 				targetCraftingSpeed = 0
 			}
-			materialRecipe := GetRecipeForItem(*material.Name, targetCraftingSpeed, *item.Name)
-			recipes = append(recipes, materialRecipe...)
+			if material.Name != nil && item.Name != nil {
+				materialRecipe := GetRecipeForItem(*material.Name, targetCraftingSpeed, *item.Name)
+				recipes = append(recipes, materialRecipe...)
+			}
 		}
 	}
 
