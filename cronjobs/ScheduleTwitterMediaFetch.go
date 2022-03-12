ba2ae2ff1ada0e9a0441851c44f199dc17f184fe
CrazyFluff <alexnou@gmail.com>
2022-03-12 02:45:19 -0700

    Get inArt Twitter Media when scheduling

--- cronjobs/ScheduleTwitterMediaFetch.go
+++ cronjobs/ScheduleTwitterMediaFetch.go
@@ -11,28 +11,33 @@ import (
 )
 
 func ScheduleTwitterMediaFetch() *cron.Cron {
+	getInArtTwitterMedia()
 
 	c := cron.New()
 	spec := "0 */5 * * * *"
 	c.AddFunc(spec, func() {
-		query := `"#inART" has:media -is:retweet`
-
-		opts := twitter.TweetRecentSearchOpts{
-			MediaFields: []twitter.MediaField{twitter.MediaFieldURL, twitter.MediaFieldType, twitter.MediaFieldPublicMetrics, twitter.MediaFieldMediaKey, twitter.MediaFieldHeight, twitter.MediaFieldWidth},
-			Expansions:  []twitter.Expansion{twitter.ExpansionEntitiesMentionsUserName, twitter.ExpansionAuthorID, twitter.ExpansionAttachmentsMediaKeys},
-			TweetFields: []twitter.TweetField{twitter.TweetFieldCreatedAt, twitter.TweetFieldConversationID, twitter.TweetFieldAttachments, twitter.TweetFieldSource, twitter.TweetFieldAuthorID, twitter.TweetFieldPossiblySensitve},
-		}
-
-		twitterMedia, err := data.GetMostRecentTwitterMedia()
-		if err != nil {
-			log.Println(err)
-		} else {
-			opts.SinceID = twitterMedia.TweetId
-			twitterapi.GetTwitterMedia(query, opts)
-		}
+		getInArtTwitterMedia()
 	})
 	c.Start()
 
 	log.Printf("Scheduled Twitter Media fetch for every 5 minutes (%v)\n", spec)
 	return c
+}
+
+func getInArtTwitterMedia() {
+	query := `"#inART" has:media -is:retweet`
+
+	opts := twitter.TweetRecentSearchOpts{
+		MediaFields: []twitter.MediaField{twitter.MediaFieldURL, twitter.MediaFieldType, twitter.MediaFieldPublicMetrics, twitter.MediaFieldMediaKey, twitter.MediaFieldHeight, twitter.MediaFieldWidth},
+		Expansions:  []twitter.Expansion{twitter.ExpansionEntitiesMentionsUserName, twitter.ExpansionAuthorID, twitter.ExpansionAttachmentsMediaKeys},
+		TweetFields: []twitter.TweetField{twitter.TweetFieldCreatedAt, twitter.TweetFieldConversationID, twitter.TweetFieldAttachments, twitter.TweetFieldSource, twitter.TweetFieldAuthorID, twitter.TweetFieldPossiblySensitve},
+	}
+
+	twitterMedia, err := data.GetMostRecentTwitterMedia()
+	if err != nil {
+		log.Println(err)
+	} else {
+		opts.SinceID = twitterMedia.TweetId
+		twitterapi.GetTwitterMedia(query, opts)
+	}
 }
