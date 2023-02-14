45bf840e3e8f4cc8dea07f44c374527ff1a7cb77
Alex Nou <alexnou@gmail.com>
2023-02-13 21:41:19 -0700

    fix: allow cors post and delete method

--- internal/middleware/CORSMiddleware.go
+++ internal/middleware/CORSMiddleware.go
@@ -5,7 +5,7 @@ import "net/http"
 func CORSMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, DELETE")
 		next(w, r)
 	}
 }
