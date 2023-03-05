f01dbaf87cac34fc0ba19936b73530ad7941e3dc
Alex Nou <alexnou@gmail.com>
2023-03-04 20:30:23 -0700

    fix: add more allowed origins

--- internal/middleware/CORSMiddleware.go
+++ internal/middleware/CORSMiddleware.go
@@ -4,8 +4,16 @@ import "net/http"
 
 func CORSMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, DELETE, OPTIONS")
+		origin := r.Header.Get("Origin")
+		allowedOrigins := []string{"http://localhost:3000", "https://nekofluff.github.io"}
+
+		for _, allowedOrigin := range allowedOrigins {
+			if origin == allowedOrigin {
+				w.Header().Set("Access-Control-Allow-Origin", "")
+				w.Header().Set("Access-Control-Allow-Methods", "POST, GET, DELETE, OPTIONS")
+			}
+		}
+
 		next(w, r)
 	}
 }
